bd94e946709e765f77e193b0e34ee52ab489f61d
futsalboss <arnoldwillsobong@gmail.com>
2021-12-16 12:00:35 +0100

    commit message

---
+++ code.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+print("hello, welcome to Nolly.co password generator!")
+length = int(input("Enter the length of password you desire:"))
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+all = lower + upper + num + symbols
+temp = random.sample(all, length)
+
+password = "".join(temp)
+print(password)
